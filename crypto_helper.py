f1102d44ee54affb99220f708a60f894c0b54ec5
houkii <jastrzebski.j.m@gmail.com>
2023-04-19 09:11:50 -0400

    Added colored bars to show current asset dynamics

--- crypto_helper.py
+++ crypto_helper.py
@@ -1,6 +1,9 @@
 import time
 import requests
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 import os
 
 # Define API endpoint and parameters
@@ -16,50 +19,77 @@ ax.set_ylabel('Price change (1h)')
 
 # Initialize dictionary to cache prices
 prices_cache = {}
-
 total_cache = {}
 
+grey = "#8C8C8C"
+green = "#00FF00"
+red = "#FF0000"
+
+stablecoin = "USDT"
+
 class Coin:
-    def __init__(self, symbol, last_price, price_change_percent):
-        self.symbol = symbol
+    def __init__(self, name, last_price, change, start_price=None):
+        self.name = name
         self.last_price = last_price
-        self.price_change_percent = price_change_percent
+        self.start_price = start_price or last_price
+        self.change_percent = 0
+        self.startup_percent_change = change
+        self.current_percent_change = change
+        
+    def update_price(self, new_price, percent_change):
+        
+        self.last_price = new_price
+        self.current_percent_change = percent_change
+        self.change_percent =  (1 - (self.start_price / new_price)) * 100
 
-    def __repr__(self):
-        return f"Coin({self.symbol}, {self.last_price}, {self.price_change_percent})"
-
-def get_coins(data):
-    coins = []
+def update_cache(cache, data, print_=False):
     for coin in data:
-        symbol = coin['symbol']
-        last_price = float(coin['lastPrice'])
-        price_change_percent = float(coin['priceChangePercent'])
-        coins.append(Coin(symbol, last_price, price_change_percent))
-
-    return coins
-
-def update_cache(cache, data, print=False):
-    # Print data for each coin
-    for coin in data:
-        name = coin['symbol']
-        price_change = coin['priceChangePercent']
-        last_price = coin['lastPrice']
-        if print:
-            print(f"Name: {name}")
-            print(f"Current price: {last_price}")
-            print(f"Price change: {price_change}%")
+        name, last_price, price_change = coin['symbol'], float(coin['lastPrice']), float(coin['priceChangePercent'])
+        if print_:
+            print(f"Name: {name}\nCurrent price: {last_price}\nPrice change: {price_change}%")
+        
+        coin_obj = Coin(name.replace(stablecoin, ""), last_price, price_change)
         
         if name not in cache:
-            cache[name] = {'start_price': last_price, 'change_percent': 0}
+            cache[name] = coin_obj;
         else:
-            start_price = cache[name]['start_price']
-            change = 1 - (float(start_price) / float(last_price))
-            cache[name]['change_percent'] = change * 100
-            formatted_change = "{:.3f}".format(change)
-            if print:
-                print(f"start_price: {start_price}")
-                print(f"diff since start: {formatted_change}%\n")    
+            cache[name].update_price(last_price, price_change)
+        
+        if print_:
+            print(f"start_price: {coin_obj.start_price}\ndiff since start: {coin_obj.change_percent}%\n")
 
+def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
+    c1=np.array(mpl.colors.to_rgb(c1))
+    c2=np.array(mpl.colors.to_rgb(c2))
+    return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
+
+def plot_data(coins):
+
+    ax.clear()
+
+    # Add last price and percentage change to end of each bar
+    for i, coin in enumerate(coins):
+
+        # Set the color depending on the value of the change percent
+        if coin.change_percent > 0:
+            color = colorFader(grey, green, min(coin.change_percent, 5) / 5)
+        else:
+            color = colorFader(grey, red, max(coin.change_percent, -5) / -5)
+
+        bar = ax.bar(coin.name, coin.current_percent_change, color=color)
+
+        # Add text to the top of the bar
+        ax.text(coin.name, coin.change_percent, "{:.1f}%".format(coin.change_percent), ha='center', va='bottom')
+        ax.text(i, bar[0].get_height() - 1 + int(math.copysign(2, coin.current_percent_change)), f"{coin.last_price:.4f}\n{coin.current_percent_change:.1f}%", ha='center', fontsize=8)
+
+    # Set y-axis limits
+    ax.set_ylim(min(coin.current_percent_change for coin in coins) - 5, max(coin.current_percent_change for coin in coins) + 5)
+    ax.set_title(stablecoin);
+
+    # Rotate x-axis labels for readability
+    plt.xticks(rotation=90)
+    plt.show()
+    plt.pause(2)
 
 while True:
     try:
@@ -67,59 +97,22 @@ while True:
         response = requests.get(url)
         data = response.json()
 
-
-        coins = get_coins(data)
-        usdt_coins = [coin for coin in coins if "usdt" in coin.symbol.lower()]
-
-        print(usdt_coins)
-
-        filtered_data = [coin for coin in data if (float(coin['priceChangePercent']) < -10 or float(coin['priceChangePercent']) > 10) and "usdt" in coin['symbol'].lower()]
+        filtered_data = [coin for coin in data if (float(coin['priceChangePercent']) < -10 or float(coin['priceChangePercent']) > 10) and stablecoin in coin['symbol']]
         sorted_data = sorted(filtered_data, key=lambda p: p['priceChangePercent'])
 
-        data_without_small_coins = [coin for coin in data if float(coin['lastPrice']) > 0.000001 and "usdt" in coin['symbol'].lower()]
+        data_without_small_coins = [coin for coin in data if float(coin['lastPrice']) > 0.000001 and stablecoin in coin['symbol']]
 
         update_cache(prices_cache, sorted_data)
         update_cache(total_cache, data_without_small_coins)
 
         os.system('clear')
-        sorted_total_cache = sorted(total_cache.items(), key=lambda x: abs(x[1]['change_percent']), reverse=True)
-        for entry in sorted_total_cache[:20]:
-            print(entry)
+        sorted_total_cache = sorted(total_cache.values(), key=lambda x: abs(x.change_percent), reverse=True)
+        sorted_total_cache = sorted(sorted_total_cache[:20], key=lambda x: x.change_percent, reverse=True)
+        for coin in sorted_total_cache:
+            print(f"{coin.name}: {coin.last_price} ({round(coin.change_percent, 1)}%)")
 
-        # Extract coin names and price change percentage for plotting
-        names = [coin['symbol'] for coin in sorted_data]
-        changes = [float(coin['priceChangePercent']) for coin in sorted_data]
-        prices = [float(coin['lastPrice']) for coin in sorted_data]
-        diff_start = [prices_cache[name]['change_percent'] for name in names]
-
-        # Clear previous plot and create new one
-        ax.clear()
-        # bars = ax.bar(names, changes)
-
-        bars = []
-        # Add last price and percentage change to end of each bar
-        for i, change in enumerate(changes):
-            color = 'r'
-            if diff_start[i] > 0:
-                color = 'g'
-
-            bar = ax.bar(names[i], change, color=color)
-
-            # Add text to the top of the bar
-            ax.text(names[i], diff_start[i], "{:.1f}%".format(diff_start[i]), ha='center', va='bottom')
-
-            ax.text(i, bar[0].get_height() + 1, f"{prices[i]:.4f}\n{changes[i]:.1f}%", ha='center', fontsize=8)
-
-        # Set y-axis limits
-        ax.set_ylim(min(changes) - 5, max(changes) + 5)
-
-        # Rotate x-axis labels for readability
-        plt.xticks(rotation=90)
-
-        # Show plot
-        plt.show()
-
-        plt.pause(3)
+        sorted_prices_cache = sorted(prices_cache.values(), key=lambda x: (x.current_percent_change > 0, abs(x.current_percent_change)), reverse=True)
+        plot_data(sorted_prices_cache)
         time.sleep(1)
 
     except Exception as e:
