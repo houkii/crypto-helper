186b08f6402515c62d36511ae67484c08cb77083
houkii <jastrzebski.j.m@gmail.com>
2023-04-19 12:04:30 -0400

    Added subplot for runtime dynamics

--- crypto_helper.py
+++ crypto_helper.py
@@ -13,9 +13,8 @@ url = 'https://api.binance.com/api/v3/ticker/24hr'
 plt.ion()
 
 # Initialize plot
-fig, ax = plt.subplots()
-ax.set_xlabel('Name')
-ax.set_ylabel('Price change (1h)')
+fig, axs = plt.subplots(2, 1, figsize=(10, 8), sharex=False)
+fig.subplots_adjust(hspace=0.5)
 
 # Initialize dictionary to cache prices
 prices_cache = {}
@@ -63,33 +62,58 @@ def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0)
     c2=np.array(mpl.colors.to_rgb(c2))
     return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
 
-def plot_data(coins):
+def get_color(value, _min = -5, _max = 5):
+    if value > 0:
+        color = colorFader(grey, green, min(value, _max) / _max)
+    else:
+        color = colorFader(grey, red, max(value, _min) / _min)
+    return color
 
-    ax.clear()
+def plot_data(volatile, dynamics):
+
+    plot_volatile(volatile, axs[0])
+    plot_dynamics(dynamics, axs[1])
+
+    # Rotate x-axis labels for readability
+    for ax in axs.flat:
+        plt.sca(ax)
+        plt.xticks(rotation=45)
+
+    plt.show()
+    plt.pause(2)
+
+def plot_volatile(coins, container):
+    container.clear()
 
     # Add last price and percentage change to end of each bar
     for i, coin in enumerate(coins):
 
         # Set the color depending on the value of the change percent
-        if coin.change_percent > 0:
-            color = colorFader(grey, green, min(coin.change_percent, 5) / 5)
-        else:
-            color = colorFader(grey, red, max(coin.change_percent, -5) / -5)
+        color = get_color(coin.change_percent)
 
-        bar = ax.bar(coin.name, coin.current_percent_change, color=color)
+        bar = container.bar(coin.name, coin.current_percent_change, color=color)
 
         # Add text to the top of the bar
-        ax.text(coin.name, coin.change_percent, "{:.1f}%".format(coin.change_percent), ha='center', va='bottom')
-        ax.text(i, bar[0].get_height() - 1 + int(math.copysign(2, coin.current_percent_change)), f"{coin.last_price:.4f}\n{coin.current_percent_change:.1f}%", ha='center', fontsize=8)
+        container.text(coin.name, coin.change_percent, "{:.1f}%".format(coin.change_percent), ha='center', va='bottom')
+        container.text(i, bar[0].get_height() - 1 + int(math.copysign(2, coin.current_percent_change)), f"{coin.last_price:.3f}\n{coin.current_percent_change:.1f}%", ha='center', fontsize=8)
 
-    # Set y-axis limits
-    ax.set_ylim(min(coin.current_percent_change for coin in coins) - 5, max(coin.current_percent_change for coin in coins) + 5)
-    ax.set_title(stablecoin);
+    
+    # Set y-containeris limits
+    container.set_ylim(min(coin.current_percent_change for coin in coins) - 5, max(coin.current_percent_change for coin in coins) + 5)
+    container.set_title(f"most volatile - {stablecoin}")
 
-    # Rotate x-axis labels for readability
-    plt.xticks(rotation=90)
-    plt.show()
-    plt.pause(2)
+def plot_dynamics(coins, container):
+    container.clear()
+
+    for i, coin in enumerate(coins):
+
+        color = get_color(coin.change_percent)
+        bar = container.bar(coin.name, coin.change_percent, color=color)
+        container.text(coin.name, coin.change_percent, "{:.1f}%".format(coin.change_percent), ha='center', va='bottom')
+        container.text(i, bar[0].get_height() - 1 + int(math.copysign(4, coin.change_percent)), f"{coin.last_price:.3f}", ha='center', fontsize=8)
+
+    container.set_ylim(min(coin.current_percent_change for coin in coins) - 5, max(coin.current_percent_change for coin in coins) + 5)
+    container.set_title(f"most dynamic - {stablecoin}")
 
 while True:
     try:
@@ -112,7 +136,7 @@ while True:
             print(f"{coin.name}: {coin.last_price} ({round(coin.change_percent, 1)}%)")
 
         sorted_prices_cache = sorted(prices_cache.values(), key=lambda x: (x.current_percent_change > 0, abs(x.current_percent_change)), reverse=True)
-        plot_data(sorted_prices_cache)
+        plot_data(sorted_prices_cache, sorted_total_cache)
         time.sleep(1)
 
     except Exception as e:
